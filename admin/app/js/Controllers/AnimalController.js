58cbea314610018c1a096d52d4fe2281eb481428
Henrique Batista <henriquecebrac37@gmail.com>
2018-06-28 20:33:04 +0000

    Ajuste cadastro animal

--- admin/app/js/Controllers/AnimalController.js
+++ admin/app/js/Controllers/AnimalController.js
@@ -2,14 +2,14 @@
       'use strict';
       angular.module('miaudote.controller')
 
-          .controller('AnimalController', function CadAnimalController($scope, $http, $location, Toast) {
-              
-               $scope.CadastrarAnimal = function() {
+          .controller('AnimalController', function CadAnimalController($scope, Menu, CheckSession, Toast, $http, $location) {
+
+              $scope.CadastrarAnimal = function() {
                   $http({
                       method: 'POST',
                       url: "../api/Animal.php?acao=CadastrarAnimal",
-                      data: {'dados': $scope.animal}
-                      }).then(function successCallback(response) {
+                      data: { 'dados': $scope.animal }
+                  }).then(function successCallback(response) {
                       if (response.data.sucesso) {
                           Toast.ShowMessage("success", response.data.mensagem);
                           $location.path("/Animal");
@@ -19,6 +19,25 @@
                       }
                   }, function errorCallback(response) {
                       Toast.ShowMessage("error", response);
+                  });
+              }
+
+              $scope.ListarAnimais = function() {
+                  Menu.LoadMenu();
+                  $http({
+                      method: 'GET',
+                      url: '../api/Animal.php?acao=BuscarTodos='
+                  }).then(function successCallback(response) {
+                      var e = response.data;
+                      if (e.sucesso) {
+                          $scope.Animal = e.data;
+                      }
+                      else {
+
+                      }
+                  }, function errorCallback(response) {
+                      //alert("aqui");
+                      // Toast.ShowMessage("error", e.mensagem);
                   });
               }
 
